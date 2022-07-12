f196cc1a0584504780d2742fabd4b4f323d0c7c7
sekarfeb <sekarfeb@gmail.com>
2022-07-12 15:32:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,19 @@
+provider "aws" {
+  access_key = "AKIA5SXAAVYWETRT2ZCC"
+  secret_key = "jL8X6Xunl9BhWOwiRR+j54KxDISGX7Gyf4YiA0zd"
+  region     = "us-west-2"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0341aeea105412b57"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "sekar-img"
+  }
+
+}
+
 output "jenkins_terraform" {
   value = "running Terraform from Jenkins"
 }
